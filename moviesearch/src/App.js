7908229af3723d4daf8be48e2e289bfb509c4090
0619jua <0619jua@naver.com>
2021-08-19 02:45:45 +0900

    moviesearch 연동

--- moviesearch/src/App.js
+++ moviesearch/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-
+import React from 'react';
+import MovieSearch from './Movie_Search/MovieSearch';
 
 function App() {
   return (
     <div className="App">
-      
+      <MovieSearch />
     </div>
   );
 }
